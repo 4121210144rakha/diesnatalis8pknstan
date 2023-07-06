a38baf85e3db90dcb1b41368e8c4a61e979d4a03
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-06 07:55:52 +0700

    #

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 export default function Loading() {
     return(
         <React.Fragment>
-            <div className="bg-inherit w-screen h-screen z-50">
-                <Image src={"/loading.webp"} priority alt="loading" width={30} height={30} className="animate-spin  top-1/2 left-1/2 absolute"/>
+            <div className="bg-inherit flex items-center justify-center w-screen h-screen z-50">
+                <Image src={"/loading.webp"} priority alt="loading" width={30} height={30} className="animate-spin"/>
             </div>
         </React.Fragment>
     )
