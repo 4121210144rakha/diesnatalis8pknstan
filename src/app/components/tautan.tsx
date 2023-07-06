7af5b9f137d7d1e8ad1ae10d489cb6daadaff8c5
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-06 08:51:34 +0700

    #

--- src/app/components/tautan.tsx
+++ src/app/components/tautan.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 export default function Tautan(props:any){
     return(
         <div className="group mt-6">
-            <Link href={`${props.url}`} rel="noopener noreferrer" className="group-hover:underline text-blue-500 w-full p-2">{props.text} &gt;</Link>
+            <Link href={`${props.url}`} rel="noopener noreferrer" className="group-hover:underline text-blue-500 w-full p-2"><p>{props.text} &gt;</p></Link>
         </div>
     )
 }
