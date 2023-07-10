5bf6b1bfbf7ce19368151f716ff5b922bd09b332
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-10 12:00:33 +0700

    #

---
+++ src/app/section/Polling.tsx
@@ -0,0 +1,14 @@
+const Polling = () => {
+    return(
+        <section id="Crack the Code" className="py-8">
+            <h2 className="text-5xl" style={{fontFamily:"Crima"}}><strong>Crack the Code</strong></h2>
+            <p className="py-8">
+                Get ready for an upcoming video puzzle experience that will leave you on the edge of your seat!
+            </p>
+                
+            
+        </section>
+    );
+}
+
+export default Polling;
