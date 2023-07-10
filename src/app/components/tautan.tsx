7487c1d94f937045ce733a17e2385095abf700f5
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-10 12:49:19 +0700

    #

--- src/app/components/tautan.tsx
+++ src/app/components/tautan.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Tautan = (props:any) => {
     return(
-        <div className="cursor-pointer group:" onClick={()=>{}} style={{ cursor: 'pointer' }}>
+        <div className="cursor-pointer group" onClick={()=>{}} style={{ cursor: 'pointer' }}>
             <Link href={`${props.alamat}`} rel="noopener noreferrer" className=" group-hover:-translate-y-56 transition duration-500 ease-in-out cursor-pointer hover:underline text-blue-500 w-full p-2"><p>{props.text} &gt;</p></Link>
         </div>
     )
