7dfef90e8fc60b70d436861285b46aaefe57d7f5
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-13 00:40:19 +0700

    #

--- src/app/awarding/page.tsx
+++ src/app/awarding/page.tsx
@@ -12,8 +12,8 @@ const Awarding = () => {
     }, []);
     return(
         isLoad?(<Loading/>):(
-            <div className='w-screen h-screen items-center justify-center'>
-                <h1 className='text-2xl font-semibold'>THANK YOU FOR YOUR VOTE!!</h1>
+            <div className='w-screen h-screen items-center flex justify-center text-center'>
+                <h1 className='text-2xl font-semibold text-center'>THANK YOU FOR YOUR VOTE!!</h1>
             </div>
         )
     );
