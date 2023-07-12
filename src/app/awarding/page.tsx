a367e31a9b2f09466df5bca001a00ebaef13f3d1
Rakha Mulia Pratama <65429650+gingercat-pisqr@users.noreply.github.com>
2023-07-12 13:27:22 +0700

    Update page.tsx

--- src/app/awarding/page.tsx
+++ src/app/awarding/page.tsx
@@ -12,9 +12,7 @@ const Awarding = () => {
     }, []);
     return(
         isLoad?(<Loading/>):(
-            <div className='w-screen h-screen items-center justify-center'>
-                <h1 className='text-2xl font-semibold'>THANK YOU FOR YOUR VOTE!!</h1>
-            </div>
+            <iframe src="https://www.menti.com/alatrybzcrhx" className="w-screen h-screen scrollbar-hide mask-image-to-bottom-for-iframe"></iframe>
         )
     );
 }
