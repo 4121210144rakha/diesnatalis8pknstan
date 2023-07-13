1f0d86f62ab3c0d4ca092adca2f55d69ea8952c9
gingercat-pisqr <rakhamuliapratama@gmail.com>
2023-07-13 07:38:43 +0700

    #

--- src/app/components/episodecard.tsx
+++ src/app/components/episodecard.tsx
@@ -28,7 +28,7 @@ export default function Episodecard(props: any) {
 
     return(
         <div ref={fadeIn} className="fade-container hover:scale-105 sm:w-96 pb-10 my-8 col-auto rounded-lg overflow-hidden ring-transparent transition ring-1 duration-300 ease-in-out hover:ring-gray-400">
-            <iframe src="https://drive.google.com/file/d/1tmFT2zicsTf1pmvc0qEZfLYmHbKusTBu/preview" title='Trailer Video Puzzle' className="w-96 h-64"></iframe>
+            <iframe src="https://drive.google.com/file/d/1tmFT2zicsTf1pmvc0qEZfLYmHbKusTBu/preview" title='Trailer Video Puzzle' className="w-full h-64"></iframe>
             <p className="pt-8"><small>13 July 2023</small></p>
             <h3 className="text-2xl pt-4 pb-10"><strong>The Silent Vow</strong></h3>
             <p className="px-4">Perjalanan batin yang penuh refleksi dan janji. Temukan maknamu dan ungkap di Malam Mahakarya</p>
--- src/app/section/movie.tsx
+++ src/app/section/movie.tsx
@@ -4,7 +4,7 @@ import EpisodeCard from '../components/episodecard'
 const Movie = () => {
     return(
         <section id="Crack the Code" className="py-8">
-            <h2 className="text-5xl" style={{fontFamily:"Crima"}}><strong>Crack the Code</strong></h2>
+            <h2 className="text-5xl" style={{fontFamily:"Crima"}}><strong>Video Puzzle</strong></h2>
             <p className="py-8">
                 Get ready for an upcoming video puzzle experience that will leave you on the edge of your seat!
             </p>
